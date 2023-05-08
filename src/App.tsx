acabaa90bead966d864c842fb5f614c48ce99961
Daniel Suen <danielsuen200791357@gmail.com>
2023-05-08 02:21:25 -0400

    created basic about page and tackled configuration errors for ts, eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import About from './components/about';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
