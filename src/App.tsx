75781777eadf4e311430816ab97f9ded52414cb0
Daniel Suen <danielsuen200791357@gmail.com>
2023-05-08 03:19:10 -0400

    added contact form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import About from './components/about';
+import Contact from './components/contact';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/contact.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './contact.scss';
+
+export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const onSubmit = function () {
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <div className="contact">
+      <h1>Feel Free to Reach Out!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Message:
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
