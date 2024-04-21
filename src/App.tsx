77f9fb59b8e5507cd62ac0aeaef929e0fb3f6576
dan-suen <danielsuen200791357@gmail.com>
2024-04-21 16:16:32 -0400

    deployed to gh-pages

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Footer from "./components/footer/footer.js";
+function App() {
+  return (
+    <div className="App">
+      <p>filler</p>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
